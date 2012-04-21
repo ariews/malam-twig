86e9485808fa622e47f55d2aed1faa85abea873a
Arie W. Subagja <arie@malam.or.id>
2012-04-21 12:01:19 +0700

    path updated

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ defined('SYSPATH') or die('No direct script access.');
  */
 
 // Load the Twig class autoloader
-require Kohana::find_file('vendor', 'Twig/Autoloader');
+require Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
 
 // Register the Twig class autoloader
 Twig_Autoloader::register();
